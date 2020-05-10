892fe48cd778844b25ba01b64747aa1bc4d2c62e
Toru Shimogaisho <gaithimo@gmail.com>
2020-05-10 23:56:02 +0000

    package entry-siteを作成

---
+++ packages/entry-site/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json", ".mjs", ".wasm"],
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, "src/public/index.html"),
+    }),
+  ],
+  devServer: {
+    host: "127.0.0.1",
+    disableHostCheck: true,
+  },
+}
