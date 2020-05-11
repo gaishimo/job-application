01753ea555a9dc6a17b7e275cee8ec72456749b2
Toru Shimogaisho <gaithimo@gmail.com>
2020-05-11 05:07:54 +0000

    Add core package

---
+++ packages/core/src/models/Job.ts
@@ -0,0 +1,10 @@
+export type Job = {
+  id: string
+  name: string
+}
+
+export const JOB_DATA: Job[] = [
+  { id: "clearicalPosition", name: "事務職" },
+  { id: "enginner", name: "エンジニア" },
+  { id: "designer", name: "デザイナー" },
+]
