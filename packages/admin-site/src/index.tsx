e93d7b00c739966f7f932ebb39e0bb80e88ccfee
Toru Shimogaisho <gaithimo@gmail.com>
2020-05-12 23:35:39 +0000

    Add admin-site package

---
+++ packages/admin-site/src/index.tsx
@@ -0,0 +1,5 @@
+/** @jsx jsx */
+import ReactDOM from "react-dom"
+import { jsx } from "@emotion/core"
+
+ReactDOM.render(<h1>応募管理サイト</h1>, document.getElementById("application"))
---
+++ packages/admin-site/webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack")
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+require("dotenv").config()
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json", ".mjs", ".wasm"],
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, "public/index.html"),
+    }),
+    new CopyPlugin([
+      {
+        from: "public/*.css",
+        to: ".",
+        flatten: true,
+      },
+    ]),
+    new webpack.DefinePlugin({
+      "process.env.APP_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development",
+      ),
+      "process.env.API_URL_ADD_JOB_ENTRY": JSON.stringify(
+        process.env.API_URL_ADD_JOB_ENTRY,
+      ),
+    }),
+  ],
+  devServer: {
+    host: "127.0.0.1",
+    disableHostCheck: true,
+    contentBase: path.resolve(__dirname, "public"),
+  },
+}
