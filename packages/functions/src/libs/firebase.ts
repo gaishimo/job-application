03119b11dc9c121282e7672fa1e9b88bdc262760
Toru Shimogaisho <gaithimo@gmail.com>
2020-05-15 01:05:35 +0000

    functions: Remove console.log

--- packages/functions/src/libs/firebase.ts
+++ packages/functions/src/libs/firebase.ts
@@ -3,8 +3,6 @@ import admin from "firebase-admin"
 export function getFirebaseAdminApp() {
   try {
     admin.initializeApp()
-  } catch (e) {
-    console.log(e)
-  }
+  } catch (e) {}
   return admin
 }
